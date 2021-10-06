8ed2027656100e8a70f5ca9372be41cce3e5bc9c
Raheem <raheemideadev@gmail.com>
2021-10-06 22:39:14 +0500

    demo app

--- app/src/main/java/xyz/teamgravity/checkinternetdemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/checkinternetdemo/MainActivity.kt
@@ -1,11 +1,30 @@
 package xyz.teamgravity.checkinternetdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import xyz.teamgravity.checkinternetdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        button()
+    }
+
+    private fun button() {
+        onCheck()
+    }
+
+    private fun onCheck() {
+        binding.apply {
+            checkB.setOnClickListener {
+
+            }
+        }
     }
 }
