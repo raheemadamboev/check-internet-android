3c3d3ff84102fc325b8cca3b9cf62b77d6971d4d
Raheem <raheemideadev@gmail.com>
2021-10-06 22:52:32 +0500

    demo app

--- app/src/main/java/xyz/teamgravity/checkinternetdemo/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/checkinternetdemo/MainActivity.kt
@@ -2,6 +2,7 @@ package xyz.teamgravity.checkinternetdemo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import xyz.teamgravity.checkinternet.CheckInternet
 import xyz.teamgravity.checkinternetdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +25,15 @@ class MainActivity : AppCompatActivity() {
         binding.apply {
             checkB.setOnClickListener {
 
+                stateT.text = getString(R.string.checking)
+
+                CheckInternet().check { connected ->
+                    if (connected) { // there is internet
+                        stateT.text = getString(R.string.internet)
+                    } else { // there is no internet
+                        stateT.text = getString(R.string.no_internet)
+                    }
+                }
             }
         }
     }
